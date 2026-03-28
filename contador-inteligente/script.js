b3a500a047abef4b6984cd223556027dbc35bdd6
Guilherme Alves Carchedi <guilhermealves.carchedi@gmail.com>
2026-03-28 20:18:11 -0300

    criando um mais um  mini projeto contador de numeros intelgente

---
+++ contador-inteligente/script.js
@@ -0,0 +1,57 @@
+var contador = 0;
+var display = document.getElementById("displayConteudo");
+var mensagem = document.getElementById("displayMensagem");
+var btnAdd = document.getElementById("adicionar");
+var btnReset = document.getElementById("resetar");
+var btnDiminuir = document.getElementById("diminuir");
+
+btnAdd.addEventListener("click", adicionar);
+btnReset.addEventListener("click", resetar);
+btnDiminuir.addEventListener("click", diminuir);
+
+if (contador == 0) {
+    display.style.color = "red";
+}
+
+function adicionar(){
+    contador++;
+    display.innerHTML = contador;
+
+    if (contador > 10){
+        contador = 10;
+        display.innerHTML = contador;
+        mensagem.innerHTML = "Você chegou ao limite!";
+    }
+
+    if (contador == 0) {
+        display.style.color = "red";
+    } else {
+        display.style.color = "green";
+    }
+}
+function resetar(){
+    contador = 0;
+    display.innerHTML = contador;
+
+    if (contador == 0) {
+        display.style.color = "red";
+    } else {
+        display.style.color = "green";
+    }
+}
+
+function diminuir(){
+    contador--;
+    display.innerHTML = contador;
+
+    if(contador < 0){
+        contador = 0;
+        display.innerHTML = contador;
+    }
+
+    if (contador == 0) {
+        display.style.color = "red";
+    } else {
+        display.style.color = "green";
+    }
+}
