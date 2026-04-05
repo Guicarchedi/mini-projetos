e6f0040670cf0098189a0958764888088f347439
Guilherme Alves Carchedi <guilhermealves.carchedi@gmail.com>
2026-04-05 13:26:39 -0300

    criando mini projeto modo escuro

---
+++ modo-escuro/script.js
@@ -0,0 +1,13 @@
+var btnModoEscuro = document.getElementById("btnModoEscuro");
+var btnModoClaro = document.getElementById("btnModoClaro");
+
+btnModoEscuro = addEventListener("click", modoEscuro);
+btnModoClaro = addEventListener("click", modoClaro);
+
+function modoEscuro() {
+
+}
+
+function modoClaro() {
+
+}
